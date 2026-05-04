894c808284a1cc70197aae75a0f29367b687c60c
mppesino <mppesino@up.edu.ph>
2026-05-04 22:53:46 +0800

    chore: removed unused imports

--- food_sharing/lib/screen/login_page.dart
+++ food_sharing/lib/screen/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:food_sharing/component/buttons.dart';
 import 'package:food_sharing/component/layouts.dart';
 import 'package:food_sharing/component/sections.dart';
