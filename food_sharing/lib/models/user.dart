62594f2309d795f17e968fcd9c3d308e51823dda
mppesino <mppesino@up.edu.ph>
2026-05-09 23:41:45 +0800

    refactor: moved image conversion to utils.dart

--- food_sharing/lib/models/user.dart
+++ food_sharing/lib/models/user.dart
@@ -1,26 +1,3 @@
-import 'dart:convert';
-import 'dart:typed_data';
-import 'package:image/image.dart' as img;
-import 'package:image_picker/image_picker.dart';
-
-Uint8List base64ToImage(String base64String) {
-  return base64Decode(base64String);
-}
-
-String imageToBase64(Uint8List bytes) {
-  return base64Encode(bytes);
-}
-
-Future<Uint8List> compressImage(XFile file) async {
-  final bytes = await file.readAsBytes();
-  final image = img.decodeImage(bytes)!;
-
-  final resized = img.copyResize(image, width: 300);
-
-  return Uint8List.fromList(
-    img.encodeJpg(resized, quality: 60),
-  );
-}
 
 class User {
   String? userId;
