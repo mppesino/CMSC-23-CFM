bbc4f8913689f28389e036a69d56730b9936ae23
Francine <fprembulat@up.edu.ph>
2026-05-19 11:30:36 +0800

    Fix: Added comments

--- food_sharing/lib/provider/posts_provider.dart
+++ food_sharing/lib/provider/posts_provider.dart
@@ -1,16 +1,24 @@
+// POSTS_PROVIDER.DART
+
+// ---------------------------------------------------------------------------------------
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:food_sharing/api/firebase_posts_api.dart';
 import '../models/post.dart';
+// ---------------------------------------------------------------------------------------
 
+// ---------------------------------------------------------------------------------------
+// CRUD operations for posts
 class PostsProvider with ChangeNotifier {
   final FirebasePostsApi firebaseService = FirebasePostsApi();
 
   Stream<QuerySnapshot> getPostsByInterests(List<String> interests) {
+    // if there are no interests, it returns all posts
     if (interests.isEmpty) {
       return firebaseService.getAllPosts(); 
     }
 
+    // indexing, order by createdAt (date/time)
     return FirebaseFirestore.instance
         .collection('posts')
         .where('tags', arrayContainsAny: interests)
@@ -18,6 +26,8 @@ class PostsProvider with ChangeNotifier {
         .snapshots();
   }
 
+  // real-time object tracking
+  // if a post is updated, the ui updates instantly
   Stream<DocumentSnapshot<Map<String, dynamic>>> streamPostById(String id) {
     return FirebaseFirestore.instance
         .collection('posts')
@@ -25,6 +35,7 @@ class PostsProvider with ChangeNotifier {
         .snapshots();
   }
 
+  // keeps hte app in sync
   Stream<QuerySnapshot> getAllPosts() {
     return firebaseService.getAllPosts();
   }
@@ -37,6 +48,8 @@ class PostsProvider with ChangeNotifier {
     return firebaseService.getPostsByTransaction(uid);
   }
 
+  // data deserialization
+  // fetch raw map data and onverts into post instance
   Future<Post?> getPostById(String id) async {
     final doc = await firebaseService.getPost(id);
     if (!doc.exists) return null;
@@ -45,19 +58,25 @@ class PostsProvider with ChangeNotifier {
     return Post.fromJson(data);
   }
 
+  // write operation
+  // add ----------------------------------------
   Future<void> addPost(Post post, String? uid) {
     return firebaseService.addPost(post.toJson(), uid);
   }
 
+  // edit ---------------------------------------
   Future<void> editPost(String id, Map<String, dynamic> updates) {
     return firebaseService.editPost(id, updates);
   }
 
+  // delete --------------------------------------
   Future<void> deletePost(String id) {
     return firebaseService.deletePost(id);
   }
 
+  // update ---------------------------------------
   Future<void> updatePostStatus(String id, PostStatus status) {
     return firebaseService.updatePostStatus(id, status);
   }
 }
+// ---------------------------------------------------------------------------------------
